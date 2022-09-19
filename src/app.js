dec1b7b83038ee996f02edd7bf2265baa24a3c0a
lunavamp <104728015+lunavamp@users.noreply.github.com>
2022-09-19 19:29:41 +0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,73 @@
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+                isMobile.Android()
+                || isMobile.BlackBerry()
+                || isMobile.iOS()
+                || isMobile.Opera()
+                || isMobile.Windows()
+                );
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc');
+}
+
+//click on menu
+
+const menuBar = document.querySelector('.menu-bar');
+const headerNav = document.querySelector('.header-nav');
+if (menuBar){
+    menuBar.addEventListener("click", function(e){
+        document.body.classList.toggle('_lock');
+        menuBar.classList.toggle('_active');
+        headerNav.classList.toggle('_active');
+    });
+}
+
+//header menu navigation
+const menuLinks = document.querySelectorAll('.header-link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header-container').offsetHeight;
+
+            if (menuBar.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                 menuBar.classList.remove('_active');
+                 headerNav.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
+
