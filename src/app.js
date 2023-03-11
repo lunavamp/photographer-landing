51efe626e72cc0385a5ec39943396083dce23ab3
Karina Kolesnichenko <blankfaces@yahoo.com>
2023-03-11 23:54:54 +0200

    changes added

--- src/app.js
+++ src/app.js
@@ -1,47 +1,41 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    const menuBar = document.querySelector('.menu-bar');
+    const headerNav = document.querySelector('.header-nav');
+    const menuLinks = Array.from(document.querySelectorAll('.menu-item[data-goto]'));
+    const headerContainer = document.querySelector('.header-container');
+
     $('.menu-bar').click(function() {
         $('.menu-bar').toggleClass('open-menu');
         $('.header-nav').toggleClass('open-menu');
-    });
-});
-
-$(document).ready(function() {
-    $('.menu-bar').click(function(event){
         $('.menu-bar, .header-menu').toggleClass('active');
         $('body').toggleClass('fixed-page');
     });
-});
 
-//header menu navigation
-const menuBar = document.querySelector('.menu-bar');
-const headerNav = document.querySelector('.header-nav');
-const menuLinks = document.querySelectorAll('.menu-item[data-goto]');
-if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener('click', onMenuLinkClick);
+        });
 
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header-container').offsetHeight;
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - headerContainer.offsetHeight;
 
-            if (menuBar.classList.contains('_active')) {
-                document.body.classList.remove('_lock');
-                 menuBar.classList.remove('_active');
-                 headerNav.classList.remove('_active');
+                if (menuBar.classList.contains('_active')) {
+                    document.body.classList.remove('_lock');
+                    menuBar.classList.toggle('_active');
+                    headerNav.classList.toggle('_active');
+                }
+
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: 'smooth',
+                });
+                e.preventDefault();
             }
-
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
         }
     }
-}
 
-// contact form
-
-const form = new ItcSubmitForm('form');
+    const form = new ItcSubmitForm('form');
+});
