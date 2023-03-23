f3fc1abf5b53a0560c8fc8d43aefd6f618be74b9
Karina Kolesnichenko <blankfaces@yahoo.com>
2023-03-23 21:17:20 +0200

    working on js navigation menu

--- src/app.js
+++ src/app.js
@@ -1,41 +1,24 @@
-$(document).ready(function() {
-    const menuBar = document.querySelector('.menu-bar');
-    const headerNav = document.querySelector('.header-nav');
-    const menuLinks = Array.from(document.querySelectorAll('.menu-item[data-goto]'));
-    const headerContainer = document.querySelector('.header-container');
+const menuBar = document.querySelector(".menu-bar");
+const headerNav = document.querySelector(".header-nav");
+const menuLinks = document.querySelectorAll(".menu-item");
 
-    $('.menu-bar').click(function() {
-        $('.menu-bar').toggleClass('open-menu');
-        $('.header-nav').toggleClass('open-menu');
-        $('.menu-bar, .header-menu').toggleClass('active');
-        $('body').toggleClass('fixed-page');
+menuBar.addEventListener("click", () => {
+  menuBar.classList.toggle("_active");
+  headerNav.classList.toggle("_active");
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const targetSelector = link.dataset.goto;
+    const targetSection = document.querySelector(targetSelector);
+
+    targetSection.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
 
-    if (menuLinks.length > 0) {
-        menuLinks.forEach(menuLink => {
-            menuLink.addEventListener('click', onMenuLinkClick);
-        });
-
-        function onMenuLinkClick(e) {
-            const menuLink = e.target;
-            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-                const gotoBlock = document.querySelector(menuLink.dataset.goto);
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - headerContainer.offsetHeight;
-
-                if (menuBar.classList.contains('_active')) {
-                    document.body.classList.remove('_lock');
-                    menuBar.classList.toggle('_active');
-                    headerNav.classList.toggle('_active');
-                }
-
-                window.scrollTo({
-                    top: gotoBlockValue,
-                    behavior: 'smooth',
-                });
-                e.preventDefault();
-            }
-        }
-    }
-
-    const form = new ItcSubmitForm('form');
+    headerNav.classList.remove("_active");
+  });
 });
