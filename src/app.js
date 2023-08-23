b34c66d40a9e7834054556061da637bbf0616678
Karina Kolesnichenko <blankfaces@yahoo.com>
2023-08-23 22:16:34 +0200

    css changes

--- src/app.js
+++ src/app.js
@@ -24,3 +24,23 @@ menuLinks.forEach((link) => {
     burgerIcon.classList.remove("_active");
   });
 });
+
+const images = document.querySelectorAll('img[id^="img"]');
+
+function setImageSource(images, folder) {
+  images.forEach((image, index) => {
+    image.src = `img/portfolio/${folder}-${index + 1}.jpg`;
+  });
+}
+
+function imageAll() {
+  setImageSource(images, 'photo');
+}
+
+function imageFashion() {
+  setImageSource(images, 'fashion');
+}
+
+function imagePortrait() {
+  setImageSource(images, 'portrait');
+}
